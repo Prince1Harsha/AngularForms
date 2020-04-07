3be4c76e5f6b2e74d296dfd9b6214d358f951d87
Harsha <harsha@Harshas-MacBook-Pro.local>
2020-04-07 18:23:18 +0530

    AngularForms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateDrivenFormComponent
   ],
   imports: [
     BrowserModule
